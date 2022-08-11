fb6ab9bb7e8c51a56fa69e0e758ef58f08e6a0f3
slawagn <slawagn@gmail.com>
2022-08-11 17:01:45 +0000

    add db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Question.create(
+    body: Faker::Lorem.question,
+    user_id: 1
+  )
+end
