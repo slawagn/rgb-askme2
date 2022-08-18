d2c99c74920809ef228a864d11f749d094c91b6b
slawagn <slawagn@gmail.com>
2022-08-18 11:12:42 +0000

    minor readability edits

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -34,7 +34,7 @@ class QuestionsController < ApplicationController
   end
 
   def hide
-    @question.update_attribute(:hidden, true)
+    @question.update(hidden: true)
 
     redirect_to questions_path
   end
