b4b2a3e3d93c8a69ca547f4b02d0a1a87c505fb0
slawagn <slawagn@gmail.com>
2022-08-20 12:57:40 +0000

    sign user in automatically on successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,16 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
 
-    User.create(user_params)
+    user = User.create(user_params)
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    if (user.save)
+      log_in_as(user)
+
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Что-то пошло не так!'
+
+      render :new
+    end
   end
 end
