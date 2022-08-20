b52778453af78efa672ea7994df3e892571d71ad
slawagn <slawagn@gmail.com>
2022-08-20 15:50:43 +0000

    add forgotten user controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     end
   end
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
 end
