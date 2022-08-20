58928ca7199784311c9b22540f91041ae9a4897c
slawagn <slawagn@gmail.com>
2022-08-20 13:55:19 +0000

    follow lesson 53

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,47 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    user = User.new(user_params)
-
-    if (user.save)
-      log_in_as(user)
-
+    if @user.save
+      log_in_as(@user)
       redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
     else
       flash.now[:alert] = 'Что-то пошло не так!'
-
       render :new
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Данные пользователя обновлены'
+    else
+      flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'Пользователь удалён'
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
