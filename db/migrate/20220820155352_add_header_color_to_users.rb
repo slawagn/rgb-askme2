b73bfb72e7f39488b2b850784647148f88cacd60
slawagn <slawagn@gmail.com>
2022-08-21 17:21:31 +0300

    remove two migrations cancelling each other out since i'm redeploying anyway

--- db/migrate/20220820155352_add_header_color_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddHeaderColorToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :header_color, :integer, default: 0
-  end
-end
--- db/migrate/20220821110235_remove_header_color_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveHeaderColorFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :header_color, :integer
-  end
-end
