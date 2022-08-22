f6823483c77f2ab8ea0c15b19a3b1802a0196e1c
slawagn <slawagn@gmail.com>
2022-08-22 08:53:18 +0000

    change column nullability and add unique indexes for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_21_113941) do
+ActiveRecord::Schema.define(version: 2022_08_22_082037) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body", null: false
@@ -22,13 +22,15 @@ ActiveRecord::Schema.define(version: 2022_08_21_113941) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "nickname"
-    t.string "email"
+    t.string "name", null: false
+    t.string "nickname", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
+    t.string "password_digest", null: false
     t.string "header_color"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
