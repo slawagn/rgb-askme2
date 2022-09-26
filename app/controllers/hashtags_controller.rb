ea7df2442e9c70643b9e0f2b9e0886f3a53ee7fa
slawagn <slawagn@gmail.com>
2022-09-26 15:02:01 +0000

    redirect to root when looking for nonexistent hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,7 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = Hashtag.find_by!(tag: params[:tag])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
